5bf92c20312764024ff8cf00cc0fd8f246986977
Trim Bresilla <trim.bresilla@gmail.com>
2018-06-18 00:18:46 +0200

    final_version

--- add_markers/src/add_markers_node.cpp
+++ add_markers/src/add_markers_node.cpp
@@ -2,13 +2,12 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <visualization_msgs/Marker.h>
 
-static const double POS_THRESH    = 0.05;
-static const double PICKUP_POS_X  = -6.8;
-static const double PICKUP_POS_Y  = -3.1;
-static const double DROPOFF_POS_X = -6.8;
-static const double DROPOFF_POS_Y = 4.55;
-
-static bool pickingUp = false;
+static const double THRESH  = 0.05;
+static const double PICK_X  = -6.8;
+static const double PICK_Y  = -3.1;
+static const double DROP_X  = -6.8;
+static const double DROP_Y  = 4.55;
+static bool pickingUp       = false;
 static ros::Publisher marker_pub;
 
 void addMarker(double x, double y){
@@ -16,6 +15,10 @@ void addMarker(double x, double y){
   marker.header.frame_id = "map";
   marker.ns = "marker";
   marker.id = 0;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = x;
+  marker.pose.position.y = y;
+  marker.pose.orientation.w = 1.0;
   marker.type = visualization_msgs::Marker::CUBE;
   marker.scale.x = marker.scale.y = marker.scale.z = 0.2;
   marker.color.r = 0.0f;
@@ -23,26 +26,14 @@ void addMarker(double x, double y){
   marker.color.b = 0.0f;
   marker.color.a = 1.0f;
   marker.lifetime = ros::Duration();
-
-  marker.action = visualization_msgs::Marker::ADD;
-  marker.pose.position.x = x;
-  marker.pose.position.y = y;
-  marker.pose.orientation.w = 1.0;
-
   marker_pub.publish(marker);
 }
 
 void onAmclPose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
   double x = msg->pose.pose.position.x;
   double y = msg->pose.pose.position.y;
-
-  if(!pickingUp &&
-      abs(x - PICKUP_POS_X) < POS_THRESH &&
-      abs(y - PICKUP_POS_Y) < POS_THRESH){
-    // PICKUP ZONE
-    ROS_INFO("Pick the object up!");
-
-    // Hide the marker
+  if(!pickingUp && abs(x - PICK_X) < THRESH && abs(y - PICK_Y) < THRESH){
+    ROS_INFO("Target picked up ");
     visualization_msgs::Marker marker;
     marker.header.frame_id = "map";
     marker.ns = "marker";
@@ -50,16 +41,12 @@ void onAmclPose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
     marker.header.stamp = ros::Time::now();
     marker.action = visualization_msgs::Marker::DELETE;
     marker_pub.publish(marker);
-
     pickingUp = true;
   }
-  else if(pickingUp &&
-      abs(x - DROPOFF_POS_X) < POS_THRESH &&
-      abs(y - DROPOFF_POS_Y) < POS_THRESH){
-    // DROPOFF ZONE
-    addMarker(DROPOFF_POS_X, DROPOFF_POS_Y);
+  else if(pickingUp && abs(x - DROP_X) < THRESH && abs(y - DROP_Y) < THRESH){
+    addMarker(DROP_X, DROP_Y);
     pickingUp = false;
-    ROS_INFO("Drop off the object, job done!");
+    ROS_INFO("Target dropped off");
   }
 }
 
@@ -70,18 +57,13 @@ int main(int argc, char** argv){
   ros::Subscriber pose_sub = n.subscribe("/amcl_pose", 1, onAmclPose);
   marker_pub = n.advertise<visualization_msgs::Marker>("/visualization_marker", 1);
 
-  // Wait until at least one subscriber appears
   while (marker_pub.getNumSubscribers() < 1){
-    if (!ros::ok()){
-      return 0;
-    }
+    if (!ros::ok()){ return 0; }
     ROS_WARN_ONCE("Please create a subscriber to the marker");
     ros::Duration(1.0).sleep();
   }
 
-  addMarker(PICKUP_POS_X, PICKUP_POS_Y);
-
+  addMarker(PICK_X, PICK_Y);
   ros::spin();
-
   return 0;
 }
--- pick_objects/src/pick_objects_node.cpp
+++ pick_objects/src/pick_objects_node.cpp
@@ -2,66 +2,56 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+static const double PICK_X  = -6.8;
+static const double PICK_Y  = -3.1;
+static const double DROP_X  = -6.8;
+static const double DROP_Y  = 4.55;
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  // Initialize the pick_objects node
+int main(int argc, char** argv) {
   ros::init(argc, argv, "pick_objects");
-
-  //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
-  // Wait 5 sec for move_base action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
   move_base_msgs::MoveBaseGoal goal;
-
-  // set up the frame parameters
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = -6.8;
-  goal.target_pose.pose.position.y = -3.1;
+  // PICK
+  goal.target_pose.pose.position.x = PICK_X;
+  goal.target_pose.pose.position.y = PICK_Y;
   goal.target_pose.pose.orientation.w = 1.0;
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending coordinates...");
+  ROS_INFO("sending TARGET pick-up coordinates...");
   ac.sendGoal(goal);
-
-  // Wait an infinite time for the results
   ac.waitForResult();
 
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  {
-    ROS_INFO("Reached pick-up!");
-    ROS_INFO("wait few seconds...");
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Pick-up reached!");
+    ROS_INFO("Wait few seconds to load...");
     ros::Duration(5.0).sleep();
 
-    // Defining a drop-off goal for the robot to go after pick-up
-    goal.target_pose.pose.position.x = -6.8;
-    goal.target_pose.pose.position.y = 4.5;
+    // DROP
+    goal.target_pose.pose.position.x = DROP_X;
+    goal.target_pose.pose.position.y = DROP_Y;
     goal.target_pose.pose.orientation.w = 1.0;
 
-    ROS_INFO("Sending Drop-off coordinates.......");
+    ROS_INFO("Sending TARGET drop-off coordinates...");
     ac.sendGoal(goal);
     ac.waitForResult();
 
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-      ROS_INFO("Robot reached drop-off!!");
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Drop-off reached!");
     }
-    else 
-    {
-      ROS_INFO("Failed to reach drop-off.");
+    else {
+      ROS_INFO("Drop-off failed to reach!");
     }
   }
-  else
-  {
+  else {
     ROS_INFO("Robot did not make it!");
   }
   return 0;
